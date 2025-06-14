321fede2a60b7ef57de4d90367796303be34e93f
vinhla29 <vinhla29052006@gmail.com>
2025-06-14 15:10:11 +0700

    alu_shuffle.v

---
+++ rtl/alu_shuffle.v
@@ -0,0 +1,21 @@
+module alu_shuffle (
+    input  wire [31:0] data_in,
+    input  wire        mode, // 0 = shuffle, 1 = unshuffle
+    output reg  [31:0] result
+);
+    integer i;
+
+    always @(*) begin
+        if (mode == 1'b0) begin // Shuffle
+            for (i = 0; i < 16; i = i + 1) begin
+                result[i*2]   = data_in[i];
+                result[i*2+1] = data_in[i+16];
+            end
+        end else begin // Unshuffle
+            for (i = 0; i < 16; i = i + 1) begin
+                result[i]      = data_in[i*2];
+                result[i + 16] = data_in[i*2 + 1];
+            end
+        end
+    end
+endmodule
